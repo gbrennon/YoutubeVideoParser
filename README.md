4ca22c9eafa5e05e8c5fe8513de7556575d8ce1c
gbrennon <glauberbrennon@gmail.com>
2014-07-04 20:15:49 -0300

    Tests and some improvement. Now listvideo returns a hash

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ YoutubeVideoParser
 ==================
 
 Ruby script that query youtube about a list of 'N' videos from a user 'U'
+
+This implementation is based on Google Youtube API V2 because it doesn't need
+any authentication on youtube server.
+
+Soon I'll update this to Youtube API V3
+
+Example, parse Confreaks channel and return the last 10:
+
+```ruby
+require_relative 'youtubevideoparser'
+
+y = YoutubeVideoParser.new('Confreaks', 10)
+y.listvideos
+```
+
+This will return an array of 10 RXML::Element
---
+++ test.rb
@@ -0,0 +1,25 @@
+require_relative 'youtubevideoparser'
+require 'minitest/autorun'
+
+describe "YoutubeVideoParser" do
+
+
+before do
+  @youtube = YoutubeVideoParser.new('glauberbrennon', 1)
+end
+
+  it "must be 'glauberbrennon'" do
+  assert_equal(@youtube.user, 'glauberbrennon')
+  end
+
+  it "must be 1" do
+  assert_equal(@youtube.num, 1)
+  end
+
+  it "must return a hash with video title and video URL " do
+    assert_instance_of(Hash, @youtube.listvideo)
+    assert_equal(@youtube.video.key?('Apr 5, 2014'), true)
+    assert_equal(@youtube.video['Apr 5, 2014'],"url=http://www.youtube.com/watch?v=vslqDVI9x_4;feature=youtube_gdata_player" )
+  end
+
+end
